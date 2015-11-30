8446524ef1715d5e06ea22fd0cd38c560c1beefd
Oleg Mygryn <o.migrin@gmail.com>
2015-11-30 19:29:22 +0200

    Added remote worker

---
+++ src/main/scala/remote/RemoteWorker.scala
@@ -0,0 +1,45 @@
+package remote
+
+import java.net.InetAddress
+
+import akka.actor._
+import org.apache.commons.codec.digest.Crypt
+
+object RemoteWorker extends App {
+  val system = ActorSystem("Workers")
+  val masterSubscriber = "akka.tcp://AkkaLoader@smaster:5500/user/Subscriber"
+  val keepAliveDelay = 2000 //ms
+
+  val remoteActor = system.actorOf(Props[RemoteWorker], name = "RemoteWorker")
+
+  while (true) {
+    subscribeToMaster()
+    Thread.sleep(keepAliveDelay)
+  }
+
+  def subscribeToMaster() = {
+    val masterActor = system.actorSelection(masterSubscriber)
+    val host = InetAddress.getLocalHost.getHostAddress
+
+    println("Registered Remote Worker host = " + host)
+
+    masterActor ! host
+  }
+
+}
+
+class RemoteWorker extends Actor {
+  def receive = {
+    case msg: String =>
+      execute(msg)
+      sender ! "ACK"
+  }
+
+  private def execute(hash: String): Unit = {
+    if (hash.nonEmpty) {
+      val start = System.currentTimeMillis()
+      val crypto = Crypt.crypt(hash)
+      println(s"Encrypted (${System.currentTimeMillis - start}) Ms. ")
+    }
+  }
+}
